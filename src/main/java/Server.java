da88bb5ebd3e8ce3c65ac7d14d1456861364bd48
Fábio Nunes <fabiodiogo.an111@gmail.com>
2023-04-24 12:31:49 +0100

    <chore>: alteração do código que guarda a chave pública do servidor, separando numa função própria

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -38,9 +38,7 @@ public class Server implements Runnable {
     @Override
     public void run ( ) {
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("pki/public_keys/serverPUk.key"));
-            writer.write(publicRSAKey.toString());
-            writer.close();
+            sendPUkToFile();
 
             while ( isConnected ) {
                 Socket client = server.accept ( );
@@ -74,4 +72,16 @@ public class Server implements Runnable {
         }
     }
 
+    /**
+     * Send the server's public key to the public key folder
+     */
+    public void sendPUkToFile(){
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("pki/public_keys/serverPUk.key"));
+            writer.write(publicRSAKey.toString());
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
